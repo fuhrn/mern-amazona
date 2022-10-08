2e25df03652e582b3154f3c8177a95cb98b61bba
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-10-08 12:14:57 -0300

    generate-sample-products

--- backend/server.js
+++ backend/server.js
@@ -2,37 +2,21 @@ import express from 'express';
 import data from './data.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
-const app = express();
+
 mongoose.connect(process.env.MONGODB_URI).then(() => { 
     console.log('Connected to ATLAS MongoDB');
 }).catch((error) => {
   console.log('Error connecting to ATLAS MongoDB', error.message);
 });
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products)
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+const app = express();
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
