16e6db319daf0dfc50d77f7c8245b966e71afbf3
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-10-08 08:40:30 -0300

    complete-cart-screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,33 +4,43 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
-  }
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
 };
 
-function reducer(state, action) { 
+function reducer(state, action) {
   switch (action.type) {
     case "CART_ADD_ITEM":
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
-      )
+      );
       const cartItems = existItem
         ? state.cart.cartItems.map((item) =>
-          // item._id es el id del item en el carrito, equivalente al id del producto
-          // pero newItem ya viene con quantity actualizado
+            // item._id es el id del item en el carrito, equivalente al id del producto
+            // pero newItem ya viene con quantity actualizado
             item._id === existItem._id ? newItem : item
-        )
-        // newItem is not in cartItems, so add it
-        : [...state.cart.cartItems, newItem];
+          )
+        : // newItem is not in cartItems, so add it
+          [...state.cart.cartItems, newItem];
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
-      
+    // coloco block {} para que no se confunda con el cartItems de arriba
+    case "CART_REMOVE_ITEM": {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
     default:
       return state;
   }
 }
 
-export function StoreProvider(props) { 
+export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
 
